f97301e14b31ad169d5978c20d81f8c4980fbabc
vkuznetcova <kuznetcova.a74@gmail.com>
2021-01-28 13:21:46 +0300

    дз 6 исправления
    весь функционал работает

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-sto
 
 const hostBus = new Vue();
 Vue.component('goods-search', {
-  // props: ['searchLine'],
+  data: () => {
+    return {
+      searchString: ''
+    }
+  },
   template: `
   <label> Поиск товара:
-    <input type="text" class="goods-search" v-bind:value="value" v-on:input="$emit('input', $event.target.value)">
+    <input type="text" class="goods-search" v-model:value="searchString">
   </label>
-  `
+  `,
+  watch: {
+    searchString: function (newValue) {
+      hostBus.$emit('search-string-input', newValue);
+    }
+  }
 });
 
 Vue.component('goods-item', {
@@ -27,74 +36,19 @@ Vue.component('goods-item', {
 });
 
 Vue.component('goods-list', {
-  props: ['filteredGoods'],
+  data: () => {
+    return {
+      goods: [],
+      searchString: ''
+    }
+  },
   template: `
     <div class="goods-list">
         <div class="goods-item" v-for="good in filteredGoods">
         <goods-item :good="good"></goods-item>
       </div>
     </div>
-  `
-});
-
-Vue.component('basket-goods', {
-  props: ['basketGoods', 'isVisibleBasket', 'isBasketEmpty'],
-  template: `
-  <div v-if="isVisibleBasket" class="basket">
-      <h2 v-if="isBasketEmpty"> Корзина пуста </h2>
-      <h2 v-else>Ваша корзина: </br> (товаров: {{basketCount}} на сумму {{basketSumm}})</h2>
-       <button v-if="!isBasketEmpty" class="clear-basket-btn" type="button" @click="basketGoods=[]">Очистить корзину</button>
-       <basket-item  v-for="(good, idx) in basketGoods" :good="good" :idx="idx">
-       </basket-item >
-  </div>
   `,
-  computed: {
-    basketCount() {
-      return this.basketGoods.reduce((summ, good) => summ + good.amount, 0);
-    },
-    basketSumm() {
-      return this.basketGoods.reduce((summ, good) => summ + good.amount * good.price, 0);
-    }
-  }
-});
-
-Vue.component('basket-item', {
-  props: ['good', 'idx', 'basketGood'],
-  template: `
-      <div class="basket-item">
-        <h3>Наименование: {{good.product_name}}</h3>
-        <p>Цена:{{ good.price }}</p>
-        <p>Количество: {{good.amount}}</p>
-        <p>Сумма: {{good.price*good.amount}}р.</p>
-        <button  class="del-from-basket-btn" type="button" @click="delFromBasket(idx)">Удалить</button> 
-      </div>
-  `,
-  methods: {
-    delFromBasket() {
-      hostBus.$emit('del-from-basket', this.idx);
-    }
-  }
-});
-
-const app = new Vue({
-  el: '#app',
-  template: `<div>
-  <header>
-      <goods-search v-model="searchLine"></goods-search>
-      <button class="basket-btn" type="button" @click="isVisibleBasket = !isVisibleBasket">Корзина</button>
-    </header>
-    <main>
-      <basket-goods :basketGoods="basketGoods" :isVisibleBasket="isVisibleBasket" :isBasketEmpty="isBasketEmpty" @del-from-basket="delFromBasket"></basket-goods>
-      <goods-list :filteredGoods="filteredGoods" @add-to-basket="addToBasket"></goods-list>
-    </main>
-    </div>`,
-  data: {
-    goods: [],
-    basketGoods: [],
-    searchLine: '',
-    isVisibleBasket: false,
-    basketGood: [],
-  },
   mounted() {
     this.makeGETRequest(`${API_URL}/catalogData.json`)
       .then(goods => {
@@ -122,10 +76,50 @@ const app = new Vue({
         xhr.open('GET', url, true);
         xhr.send();
       })
+    }
+  },
+  computed: {
+    filteredGoods() {
+      const search = new RegExp(this.searchString, 'i');
+      return this.good = this.goods.filter((good) => good.product_name.match(search));
+    }
+  },
+  created() {
+    hostBus.$on('search-string-input', (newValue) => this.searchString = newValue);
+  },
+  beforeDestroy() {
+    hostBus.$off('search-string-input');
+  }
+});
+
+Vue.component('basket-goods', {
+  data: () => {
+    return {
+      basketGoods: []
+    }
+  },
+  props: ['isVisibleBasket'],
+  template: `
+  <div v-if="isVisibleBasket" class="basket">
+      <h2 v-if="isBasketEmpty"> Корзина пуста </h2>
+      <h2 v-else>Ваша корзина: </br> (товаров: {{basketCount}} на сумму {{basketSumm}})</h2>
+       <button v-if="!isBasketEmpty" class="clear-basket-btn" type="button" @click="basketGoods=[]">Очистить корзину</button>
+       <basket-item  v-for="(good, idx) in basketGoods" :key="idx" :good="good" :idx="idx">
+       </basket-item >
+  </div>
+  `,
+  computed: {
+    basketCount() {
+      return this.basketGoods.reduce((summ, good) => summ + good.amount, 0);
     },
-    visibleBasket() {
-      isVisibleBasket = true
+    basketSumm() {
+      return this.basketGoods.reduce((summ, good) => summ + good.amount * good.price, 0);
     },
+    isBasketEmpty() {
+      return this.basketGoods.length === 0;
+    }
+  },
+  methods: {
     addToBasket(good) {
       if (!this.basketGoods.some((basketGood) => {
         if (basketGood.id_product === good.id_product) {
@@ -144,21 +138,6 @@ const app = new Vue({
       }
     }
   },
-  computed: {
-    filteredGoods() {
-      const search = new RegExp(this.searchLine, 'i');
-      return this.filteredGoods = this.goods.filter((good) => good.product_name.match(search));
-    },
-    // basketCount() {
-    //   return this.basketGoods.reduce((summ, good) => summ + good.amount, 0);
-    // },
-    // basketSumm() {
-    //   return this.basketGoods.reduce((summ, good) => summ + good.amount * good.price, 0);
-    // },
-    isBasketEmpty() {
-      return this.basketGoods.length === 0;
-    }
-  },
   created() {
     hostBus.$on('add-to-basket', this.addToBasket);
     hostBus.$on('del-from-basket', this.delFromBasket);
@@ -166,6 +145,41 @@ const app = new Vue({
   beforeDestroy() {
     hostBus.$off('add-to-basket');
     hostBus.$off('del-from-basket');
+  }
+});
+
+Vue.component('basket-item', {
+  props: ['good', 'idx', 'basketGood'],
+  template: `
+      <div class="basket-item">
+        <h3>Наименование: {{good.product_name}}</h3>
+        <p>Цена:{{ good.price }}</p>
+        <p>Количество: {{good.amount}}</p>
+        <p>Сумма: {{good.price*good.amount}}р.</p>
+        <button  class="del-from-basket-btn" type="button" @click="delFromBasket(idx)">Удалить</button> 
+      </div>
+  `,
+  methods: {
+    delFromBasket() {
+      hostBus.$emit('del-from-basket', this.idx);
+    }
+  }
+});
+
+const app = new Vue({
+  el: '#app',
+  template: `<div>
+  <header>
+      <goods-search></goods-search>
+      <button class="basket-btn" type="button" @click="isVisibleBasket = !isVisibleBasket">Корзина</button>
+    </header>
+    <main>
+      <basket-goods :isVisibleBasket="isVisibleBasket"></basket-goods>
+      <goods-list></goods-list>
+    </main>
+    </div>`,
+  data: {
+    isVisibleBasket: false,
   }
 });
 
